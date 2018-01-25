816e0da64f4a31016e0284e2d07e2572b18ce2dd
Foong Siu Yi <foongsiuyi@outlook.com>
2018-01-26 01:48:07 +0800

    Finalised

---
+++ app/src/main/java/com/siuyifypcaptcha/testfuncpatcha/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.siuyifypcaptcha.testfuncpatcha;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+// this is to print the verification message as a new activity
+public class MainActivity2 extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
